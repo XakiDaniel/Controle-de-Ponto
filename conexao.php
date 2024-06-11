75e84503dbf9dca99ddb1cba25ba4b5f8fcdb7b1
daniel charre <daniel.dsc993@gmail.com>
2024-06-11 08:47:27 -0300

    Criado a pagina conexao.php e configurando as info de BD

---
+++ conexao.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "xdb";
+    $port = 3306;
+
+    try {
+        //Conexao com a porta
+        # $conn = new PDO("mysql:host=$host;port=$port;dbname=$db_name, $db_user, $db_pass");
+
+        //Conexao sem a porta
+        $conn = new PDO("mysql:host=$db_host;dbname=". $db_name, $db_user, $db_pass);
+        echo "Conexao feita";
+    } catch (PDOException $e) {
+        echo "Falha" . $e->getMessage();
+    }
+
+?>
